339deeed3895d46e96a2a9b6baa84b98a360828d
Aditya Kumar <way2adityakumar@gmail.com>
2025-11-30 10:01:59 +0530

    optimized

--- src/assets/firebase.js
+++ src/assets/firebase.js
@@ -7,18 +7,5 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA_uxhFhagLWx-0wK0aL6vYLLiYHR-mR8w",
-  authDomain: "cine-pilot.firebaseapp.com",
-  projectId: "cine-pilot",
-  storageBucket: "cine-pilot.firebasestorage.app",
-  messagingSenderId: "145677621726",
-  appId: "1:145677621726:web:2ea036b11e61d60ffde54f",
-  measurementId: "G-PF6BV7Y5BR",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export let auth = getAuth();
